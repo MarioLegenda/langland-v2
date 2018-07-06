44a3935e3081eeed30711e9886d59cda1bdec14f
mario <likearollingstone199@gmail.com>
2018-07-06 12:30:31 +0100

    Created initial landing page

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,9 +1,11 @@
 import Vue from "vue";
-import {Header} from "./Header";
 import {IUserRepository} from "./Repository/Contract";
 import {RepositoryFactory} from "./Repository/Implementation";
 import {store} from "./store";
 import {User} from "./Repository/Models";
+
+import {Header as PublicHeader} from "./Public/Header";
+import {Body as PublicBody} from "./Public/Body";
 
 const userRepository: IUserRepository = RepositoryFactory.create('user');
 
@@ -11,10 +13,12 @@ userRepository.read((response) => {
     new Vue({
         el: "#vue_app",
         template: `<div class="app">
-                       <Header></Header>
+                       <PublicHeader></PublicHeader>
+                       <PublicBody></PublicBody>
                    </div>`,
         components: {
-            Header
+            PublicHeader,
+            PublicBody
         },
         beforeCreate() {
             store.addUser(new User(response.resource.data));
