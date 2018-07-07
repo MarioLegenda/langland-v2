247b73109f75015273deff053b463b6a71d80a2f
mario <likearollingstone199@gmail.com>
2018-07-07 11:32:22 +0100

    Added guide route for guides

--- src/Web/StaticController.php
+++ src/Web/StaticController.php
@@ -6,8 +6,27 @@ use Symfony\Component\HttpFoundation\Response;
 
 class StaticController
 {
-
+    /**
+     * @param \Twig_Environment $templating
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function getIndex(\Twig_Environment $templating): Response
+    {
+        $content = $templating->render('static/index.html.twig');
+
+        return new Response($content);
+    }
+    /**
+     * @param \Twig_Environment $templating
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function getGuide(\Twig_Environment $templating): Response
     {
         $content = $templating->render('static/index.html.twig');
 
